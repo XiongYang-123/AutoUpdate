79809b08460ed12f48a245b0bdd248e9998d6bfc
xiongy <853764365@qq.com>
2020-04-21 12:02:06 +0800

    基本架构

---
+++ AutoUpdate/Tools/Model.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tools
+{
+    public class UpdateConfig
+    {
+        /// <summary>
+        /// 主版本号
+        /// </summary>
+        public string Version { get; set; }
+        /// <summary>
+        /// 当前版本是否需要强制更新
+        /// </summary>
+        public bool IsForcedUpdate { get; set; }
+        /// <summary>
+        /// 需要更新的文件
+        /// </summary>
+        public List<FileConfig> Files { get; set; }
+
+    }
+    public class FileConfig
+    {
+        /// <summary>
+        /// 子文件版本号
+        /// </summary>
+        public string Version { get; set; }
+        /// <summary>
+        /// 文件目录 存放在服务器上的地址
+        /// </summary>
+        public string Path { get; set; }
+    }
+}
---
+++ AutoUpdate/Tools/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tools
+{
+    /// <summary>
+    /// 工具类
+    /// </summary>
+    public static class Utils
+    {
+       
+    }
+}
